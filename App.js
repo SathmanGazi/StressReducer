7619a6e76bf8f412299770128cec27b6a03af215
Musa Al Sathman Gazi <gazi@usf.edu>
2022-06-22 08:42:44 +0600

    Understanding the button component while using hook

--- App.js
+++ App.js
@@ -1,33 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import {
-  StyleSheet,
-  TouchableWithoutFeedback,
-  Text,
-  View,
-  TouchableHighlight,
-  Image,
-  SafeAreaView,
-} from 'react-native';
+import React from 'react';
+import { StyleSheet, SafeAreaView, Button } from 'react-native';
 
 export default function App() {
-  const handlePress = () => console.log('Text Pressed');
+  const handlePress = () => console.log('Button Tapped');
   return (
     <SafeAreaView style={styles.container}>
-      <Text numberOfLines={1} onPress={handlePress}>
-        This will be my second project!
-      </Text>
-      <Image source={require('./assets/favicon.png')} />
-      <TouchableHighlight onPress={() => console.log('Image Tapped')}>
-        <Image
-          fadeDuration={1000}
-          blurRadius={1}
-          source={{
-            width: 200,
-            height: 300,
-            uri: 'https://picsum.photos/200/300',
-          }}
-        />
-      </TouchableHighlight>
+      <Button color={'red'} title='Click me' onPress={handlePress} />
     </SafeAreaView>
   );
 }
