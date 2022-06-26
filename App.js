035eb881379831828710fab60d3813bc0a13e891
Musa Al Sathman Gazi <gazi@usf.edu>
2022-06-26 20:11:51 +0600

    Catto time

--- App.js
+++ App.js
@@ -1,38 +1,54 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, SafeAreaView, StyleSheet, Image, Text } from 'react-native';
 
 export default function App() {
   return (
-    <View
-      style={{
-        backgroundColor: '#fff',
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
+    <SafeAreaView style={styles.container}>
+      <Image source={require('./assets/catto.jpg')} />
+
+      <Image
+        source={require('./assets/cat-icon-0.png')}
+        style={styles.img_cover}
+      ></Image>
+      <Text style={(styles.img_cover, styles.text)}>
+        "Sign up Biatch. Meow meow mother fucker"
+      </Text>
+
       <View
         style={{
-          backgroundColor: 'navy',
-          width: 100,
-          height: 300,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: 'indigo',
-          width: 100,
-          height: 200,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: 'royalblue',
-          width: 100,
+          backgroundColor: 'tomato',
           height: 100,
+          alignSelf: 'stretch',
         }}
       />
-    </View>
+      <View
+        style={{
+          backgroundColor: 'blue',
+          height: 100,
+          alignSelf: 'stretch',
+        }}
+      />
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  img_cover: {
+    position: 'absolute',
+    top: 20,
+    width: 100,
+    height: 100,
+    padding: 20,
+  },
+  text: {
+    padding: 30,
+    fontWeight: 'bold',
+  },
+});
