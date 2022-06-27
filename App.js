729662d5a282697fffab88fe587e14654f44f706
Musa Al Sathman Gazi <gazi@usf.edu>
2022-06-27 10:06:28 +0600

    Playing with borders

--- App.js
+++ App.js
@@ -4,5 +4,24 @@ import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
-  return <ViewImageScreen />;
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: 'dodgerblue',
+          width: 100,
+          height: 100,
+          borderWidth: 10,
+          borderColor: 'royalblue',
+          borderRadius: 50,
+        }}
+      ></View>
+    </View>
+  );
 }
