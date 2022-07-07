054c400e0bd64583a4f482a7c8a057950611c8e3
Musa Al Sathman Gazi <gazi@usf.edu>
2022-07-07 12:43:44 +0600

    Added text input file

--- App.js
+++ App.js
@@ -1,21 +1,27 @@
-import React from 'react';
-import { View, SafeAreaView, StyleSheet, Image, Text } from 'react-native';
+import React, { useState } from 'react';
+import {
+  View,
+  SafeAreaView,
+  StyleSheet,
+  Image,
+  Text,
+  TextInput,
+} from 'react-native';
 
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
+import AccountScreen from './app/screens/AccountScreen';
+import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
+import ListingsScreen from './app/screens/ListingsScreen';
+import MessagesScreen from './app/screens/MessagesScreen';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
-
-import AppText from './app/components/AppText';
-import AppButton from './app/components/AppButton';
-import Card from './app/components/Card';
-import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
-import MessagesScreen from './app/screens/MessagesScreen';
 import Screen from './app/components/Screen';
-import Icon from './app/components/Icon';
-import ListItem from './app/components/ListItem';
-import AccountScreen from './app/screens/AccountScreen';
-import ListingsScreen from './app/screens/ListingsScreen';
+import AppTextInput from './app/components/AppTextInput';
 export default function App() {
-  return <ListingsScreen />;
+  return (
+    <Screen>
+      <AppTextInput placeholder='Username' icon='email' />
+    </Screen>
+  );
 }
